fc3c5da56705b3d60bb560a7bfb35a8c34ae72a5
Penny <macphantomsoft@gmail.com>
2024-06-17 16:37:15 +0100

    modified the css on components as well made the banner images display

--- src/app/component/slider/slider.component.ts
+++ src/app/component/slider/slider.component.ts
@@ -1,4 +1,9 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, ViewEncapsulation, Input} from '@angular/core';
+import {
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  ViewEncapsulation,
+  Input,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
@@ -9,21 +14,39 @@ import { sliderItems } from 'src/app/interface/menu-items';
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss'],
   standalone: true,
-  //encapsulation: ViewEncapsulation.None,
   imports: [IonicModule, CommonModule, FormsModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class SliderComponent   {
+export class SliderComponent {
+  constructor() {}
 
-  constructor() { }
+  images: sliderItems[] = [
+    {
+      image:'assets/images/ehud-neuhaus-wW5854gAm1c-unsplash.jpg',
+      title: 'Kaz Address ',
+      about: 'Ask the managing director',
+    },
+    {
+      image:'assets/images/cristina-gavrila-Ve_QjaM54OA-unsplash.jpg',
+      title: 'Kaz Address ',
+      about: 'Ask the managing director',
+    },
+    {
+      image:'assets/images/patrick-hendry-6xeDIZgoPaw-unsplash.jpg',
+      title: 'Kaz Address ',
+      about: 'Ask the managing director',
+    },
+    {
+      image:'assets/images/eduardo-cano-photo-co-7OML-RUYSA4-unsplash.jpg',
+      title: 'Kaz Address ',
+      about: 'Ask the managing director',
+    },
 
+    {
+      image:'assets/images/andre-robillard-IUwLkxL49co-unsplash.jpg',
+      title: 'Kaz Address ',
+      about: 'Ask the managing director',
+    },
 
- images: sliderItems[] = [
-    {image: "https://source.unsplash.com/factory-smoke-coming-out-from-factory--CwiIE7D5Qk/2000x1080", title:'Kaz Address ', about: ' Ask the managing director' },
-    {image: "https://source.unsplash.com/https://unsplash.com/photos/white-and-brown-factory-during-day-wW5854gAm1c", title:'Kaz Address ', about: ' Ask the managing director' },
-    {image: "https://cdn-img.oraimo.com/2023/12/14/banner-l1.jpg", title:'Kaz Address ', about: ' Ask the managing director' },
-    ]
-
-  
-
+  ];
 }
